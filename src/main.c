4c8dcf9847527c23da0c0503299656a5a5d60d98
Estanislao Crivos <estanislaocrivos@gmail.com>
2026-06-24 20:31:23 -0300

    feat(arp): implement ARP request handling and update MAC address definitions

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 /* ========================================================================== */
 
+#include "arp.h"
 #include "clock.h"
 #include "enc28j60.h"
 #include "errno.h"
@@ -14,100 +15,19 @@
 
 /* ========================================================================== */
 
-#define MAC_ADDR_BYTE_1         0x02
-#define MAC_ADDR_BYTE_2         0x00
-#define MAC_ADDR_BYTE_3         0x00
-#define MAC_ADDR_BYTE_4         0x00
+#define MAC_ADDR_BYTE_6         0x02
 #define MAC_ADDR_BYTE_5         0x00
-#define MAC_ADDR_BYTE_6         0x01
+#define MAC_ADDR_BYTE_4         0x00
+#define MAC_ADDR_BYTE_3         0x00
+#define MAC_ADDR_BYTE_2         0x00
+#define MAC_ADDR_BYTE_1         0x01
 
 #define ENC28J60_RXBUF_END_ADDR 0x19BF
 
-#define IPV4_ADDR_BYTE_4        192
+#define IPV4_ADDR_BYTE_4        192 /* MSB */
 #define IPV4_ADDR_BYTE_3        168
 #define IPV4_ADDR_BYTE_2        1
 #define IPV4_ADDR_BYTE_1        250
-
-bool arp_is_request_for_me(uint8_t* frame, uint16_t size)
-{
-    if (frame == NULL)
-    {
-        return -EFAULT;
-    }
-    if (size < 42)
-    {
-        return false;
-    }
-
-    uint16_t eth_type = (uint16_t)frame[12] << 8 | frame[13];
-    if (eth_type != 0x0806) /* Check if ARP */
-    {
-        return false;
-    }
-
-    uint16_t op_type = (uint16_t)frame[20] << 8 | frame[21];
-    if (op_type != 0x0001) /* Check if request */
-    {
-        return false;
-    }
-
-    return (
-        frame[38] == IPV4_ADDR_BYTE_4 && frame[39] == IPV4_ADDR_BYTE_3
-        && frame[40] == IPV4_ADDR_BYTE_2 && frame[41] == IPV4_ADDR_BYTE_1);
-}
-
-int8_t arp_reply_request(uint8_t* frame, uint16_t size)
-{
-    if (frame == NULL)
-    {
-        return -EFAULT;
-    }
-
-    /* DST field */
-    frame[0] = frame[22];
-    frame[1] = frame[23];
-    frame[2] = frame[24];
-    frame[3] = frame[25];
-    frame[4] = frame[26];
-    frame[5] = frame[27];
-
-    /* SRC field */
-    frame[6]  = MAC_ADDR_BYTE_1;
-    frame[7]  = MAC_ADDR_BYTE_2;
-    frame[8]  = MAC_ADDR_BYTE_3;
-    frame[9]  = MAC_ADDR_BYTE_4;
-    frame[10] = MAC_ADDR_BYTE_5;
-    frame[11] = MAC_ADDR_BYTE_6;
-
-    frame[20] = 0;
-    frame[21] = 2;
-
-    frame[32] = frame[0];
-    frame[33] = frame[1];
-    frame[34] = frame[2];
-    frame[35] = frame[3];
-    frame[36] = frame[4];
-    frame[37] = frame[5];
-
-    frame[22] = frame[6];
-    frame[23] = frame[7];
-    frame[24] = frame[8];
-    frame[25] = frame[9];
-    frame[26] = frame[10];
-    frame[27] = frame[11];
-
-    frame[38] = frame[28];
-    frame[39] = frame[29];
-    frame[40] = frame[30];
-    frame[41] = frame[31];
-
-    frame[28] = IPV4_ADDR_BYTE_4;
-    frame[29] = IPV4_ADDR_BYTE_3;
-    frame[30] = IPV4_ADDR_BYTE_2;
-    frame[31] = IPV4_ADDR_BYTE_1;
-
-    return 0;
-}
 
 int main(void)
 {
@@ -178,6 +98,18 @@ int main(void)
            .was_initialized = false};
     enc28j60_init(&enc28j60);
 
+    struct arp arp
+        = {.ip_addr[3]  = IPV4_ADDR_BYTE_4,
+           .ip_addr[2]  = IPV4_ADDR_BYTE_3,
+           .ip_addr[1]  = IPV4_ADDR_BYTE_2,
+           .ip_addr[0]  = IPV4_ADDR_BYTE_1,
+           .mac_addr[5] = MAC_ADDR_BYTE_6,
+           .mac_addr[4] = MAC_ADDR_BYTE_5,
+           .mac_addr[3] = MAC_ADDR_BYTE_4,
+           .mac_addr[2] = MAC_ADDR_BYTE_3,
+           .mac_addr[1] = MAC_ADDR_BYTE_2,
+           .mac_addr[0] = MAC_ADDR_BYTE_1};
+
     /* ====================================================================== */
 
     uint8_t frame[1518]; /* Where eth frame will be stored */
@@ -198,7 +130,8 @@ int main(void)
             continue;
         }
 
-        uint8_t arp_req = (uint8_t)arp_is_request_for_me(frame, sizeof(frame));
+        bool is_for_me = false;
+        arp_is_request_for_me(&arp, frame, sizeof(frame), &is_for_me);
 
         char   buf[128];
         size_t len = snprintf(
@@ -221,12 +154,12 @@ int main(void)
             frame[11],
             frame[12],
             frame[13],
-            arp_req);
+            (uint8_t)is_for_me);
         serial_log.ops->transmit(&serial_log, (uint8_t*)buf, len);
 
-        if (arp_req)
+        if (is_for_me)
         {
-            arp_reply_request(frame, sizeof(frame));
+            arp_reply_request(&arp, frame, sizeof(frame));
             enc28j60_transmit_packet(&enc28j60, frame, eth_pkt_size);
         }
     }
